7e208cb2c46d54108e746f62f6cedac9839caccc
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-22 19:53:55 -0600

    edit controller.js

--- server/controller.js
+++ server/controller.js
@@ -14,39 +14,40 @@ module.exports = {
         try {
             const { data: commands, error } = await supabase.from('commands').select('*')
             console.log(commands)
+            res.status(200).send('get got')
         } catch (error) {
             console.log(error)
         }
-    },
-    spawn: async (req, res) => {
-
-        let output = ''
-        const makeSystemCall = (command) => {
-            const systemCall = spawn(command)
-        
-            systemCall.stdout.on(`data`, (data) => {
-                output += data
-                console.log(`stdout: ${data}`)
-            })
-        
-            systemCall.stderr.on(`data`, (data) => {
-                console.error(`stderr: ${data}`)
-            })
-        
-            systemCall.on(`close`, (code) => {
-                console.log(`child process exited with code ${code}`)
-            })
-        }
-
-        try {
-            const { data: commands, error } = await supabase.from('commands').select('*')
-            console.log(commands)
-        } catch (error) {
-            console.log(error)
-        }
-    makeSystemCall('ls')
-
     }
+    // spawn: async (req, res) => {
+
+    //     let output = ''
+    //     const makeSystemCall = (command) => {
+    //         const systemCall = spawn(command)
+        
+    //         systemCall.stdout.on(`data`, (data) => {
+    //             output += data
+    //             console.log(`stdout: ${data}`)
+    //         })
+        
+    //         systemCall.stderr.on(`data`, (data) => {
+    //             console.error(`stderr: ${data}`)
+    //         })
+        
+    //         systemCall.on(`close`, (code) => {
+    //             console.log(`child process exited with code ${code}`)
+    //         })
+    //     }
+
+    //     try {
+    //         const { data: commands, error } = await supabase.from('commands').select('*')
+    //         console.log(commands)
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // makeSystemCall('ls')
+
+    // }
 }
 
 
