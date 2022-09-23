cd1b94ea377d30da6f5b92f679a83ef671b7984f
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-22 22:33:02 -0600

    jhkjh

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {SERVER_PORT} = process.env
-const {get} = require('./controller.js')
+const {get} = require('./server/controller.js')
 
 app.use(express.json())
 app.use(cors())
