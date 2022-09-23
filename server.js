410a948bed0efd05f66fd5a65ac75db35818c8d5
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-22 23:55:57 -0600

    comment out supabase instance remove try catch

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const {SERVER_PORT} = process.env
 // const { createClient } = require('@supabase/supabase-js')
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+// )
 
 app.use(express.json())
 app.use(cors())
@@ -20,13 +20,11 @@ app.get('/', (req, res) => {
 })
 
  app.get('/commands', async (req, res) => {
-        try {
+       
             const { data: commands, error } = await supabase.from('commands').select('*')
             console.log(commands)
-            res.status(200).send('get got')
-        } catch (error) {
-            console.log(error)
-        }
+            res.status(200).send('commands')
+        
     })
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
