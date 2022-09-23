44c5254fc857fdff17ec00dce4b1d9a3121dbd9f
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-22 19:55:05 -0600

    edit server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {SERVER_PORT} = process.env
-const {get, spawn} = require('./controller.js')
+const {get} = require('./controller.js')
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/get', get)
-app.get(`/spawn`, spawn)
+// app.get(`/spawn`, spawn)
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
