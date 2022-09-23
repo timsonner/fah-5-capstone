b1407e15b598ec47ce3ab1f9b6488e9e277b4ef8
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-23 00:46:52 -0600

    fix import

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {SERVER_PORT} = process.env
-const { root, commands } = require('../controller.js')
+const { root, commands } = require('./controller.js')
 
 
 app.use(express.json())
