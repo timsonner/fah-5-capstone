86f0c33cc5d39058980bb048e01d87de0a37c6a7
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-23 02:49:51 -0600

    fix endpoint move systemcall

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const { spawn } = require('child_process')
 const { createClient } = require('@supabase/supabase-js')
 const path = require('path')
 const { v4: uuidv4 } = require('uuid')
@@ -66,34 +65,5 @@ module.exports = {
         return
       }
       res.send(data)
-  }
-  // spawn: async (req, res) => {
-
-  //     let output = ''
-  //     const makeSystemCall = (command) => {
-  //         const systemCall = spawn(command)
-
-  //         systemCall.stdout.on(`data`, (data) => {
-  //             output += data
-  //             console.log(`stdout: ${data}`)
-  //         })
-
-  //         systemCall.stderr.on(`data`, (data) => {
-  //             console.error(`stderr: ${data}`)
-  //         })
-
-  //         systemCall.on(`close`, (code) => {
-  //             console.log(`child process exited with code ${code}`)
-  //         })
-  //     }
-
-  //     try {
-  //         const { data: commands, error } = await supabase.from('commands').select('*')
-  //         console.log(commands)
-  //     } catch (error) {
-  //         console.log(error)
-  //     }
-  // makeSystemCall('ls')
-
-  // }
+  }  
 }
