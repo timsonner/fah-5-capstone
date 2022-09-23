c9ae378abe276ce50a2571419364e066baf0aa86
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-23 01:11:09 -0600

    add post endpoint

--- server/controller.js
+++ server/controller.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
-const { spawn } = require("child_process")
+require('dotenv').config()
+const { spawn } = require('child_process')
 const { createClient } = require('@supabase/supabase-js')
 const path = require('path')
-
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
- 
+
 module.exports = {
   root: (req, res) => {
     try {
@@ -18,50 +17,50 @@ module.exports = {
       console.log('error sending index.html', error)
       res.sendStatus(400)
     }
-},
- commands: async (req, res) => {
-      try {
-          const { data: commands, error } = await supabase.from('commands').select('*')
-          console.log(commands)
-          res.status(200).send(commands)
-      } catch (error) {
-          console.log(error)
-      }
   },
-  
-    // spawn: async (req, res) => {
-
-    //     let output = ''
-    //     const makeSystemCall = (command) => {
-    //         const systemCall = spawn(command)
-        
-    //         systemCall.stdout.on(`data`, (data) => {
-    //             output += data
-    //             console.log(`stdout: ${data}`)
-    //         })
-        
-    //         systemCall.stderr.on(`data`, (data) => {
-    //             console.error(`stderr: ${data}`)
-    //         })
-        
-    //         systemCall.on(`close`, (code) => {
-    //             console.log(`child process exited with code ${code}`)
-    //         })
-    //     }
-
-    //     try {
-    //         const { data: commands, error } = await supabase.from('commands').select('*')
-    //         console.log(commands)
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // makeSystemCall('ls')
-
-    // }
+  getCommands: async (req, res) => {
+    try {
+      const { data: commands, error } = await supabase
+        .from('commands')
+        .select('*')
+      console.log(commands)
+      res.status(200).send(commands)
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  insertCommand: async (req, res) => {
+    const { data, error } = await supabase
+    .from('commands')
+    .insert([{ name: 'whoami'}])
 }
+  // spawn: async (req, res) => {
 
+  //     let output = ''
+  //     const makeSystemCall = (command) => {
+  //         const systemCall = spawn(command)
 
+  //         systemCall.stdout.on(`data`, (data) => {
+  //             output += data
+  //             console.log(`stdout: ${data}`)
+  //         })
 
+  //         systemCall.stderr.on(`data`, (data) => {
+  //             console.error(`stderr: ${data}`)
+  //         })
 
+  //         systemCall.on(`close`, (code) => {
+  //             console.log(`child process exited with code ${code}`)
+  //         })
+  //     }
 
+  //     try {
+  //         const { data: commands, error } = await supabase.from('commands').select('*')
+  //         console.log(commands)
+  //     } catch (error) {
+  //         console.log(error)
+  //     }
+  // makeSystemCall('ls')
 
+  // }
+}
--- server/server.js
+++ server/server.js
@@ -2,14 +2,16 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {SERVER_PORT} = process.env
-const { root, commands } = require('./controller.js')
+const { SERVER_PORT } = process.env
+const { root, getCommands, insertCommand } = require('./controller.js')
 
-
+// meedleware didi
 app.use(express.json())
 app.use(cors())
 
+// routes
 app.get('/', root)
-app.get('/commands', commands)
+app.get('/commands', getCommands)
+app.post('/commands', insertCommand)
 
 app.listen(SERVER_PORT, () => console.log(`Express() up on ${SERVER_PORT}`))
