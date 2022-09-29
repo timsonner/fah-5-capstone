902b67d10cb099a6ea7dcda5028a53eecd6e25ba
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-29 00:27:37 -0600

    test other way .data return

--- server/public/main.js
+++ server/public/main.js
@@ -77,7 +77,7 @@ const spawnCommand = async (command) => {
 
 const resolvePromise = async (promise) => {
     const returnPromise = await promise
-    return returnPromise.data
+    return returnPromise
 }
 
 
@@ -102,4 +102,4 @@ console.log(`foo output: ${foo}`)
 console.log(`foo.data output: ${foo.data}`)
 
 const bar = resolvePromise(foo)
-console.log(bar)
+console.log(bar.data)
