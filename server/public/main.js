75c196d7bff24909d5317e9846678a624c32f695
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-29 22:56:19 -0600

    comment out foreach

--- server/public/main.js
+++ server/public/main.js
@@ -74,13 +74,13 @@ commandListSection.appendChild(commandList)
 const getCommands = async () => {
     try {
       let res = await axios.get(baseURL)
-      res.forEach((element) => {
-        // create view using element
-        const commandListItem = document.createElement('li')
-          commandList.appendChild(commandListItem)
-          commandListItem.textContent = JSON.stringify(element.data)
-      })
-      console.log(`🟢 getCommands()`)
+    //   res.forEach((element) => {
+    //     // create view using element
+    //     const commandListItem = document.createElement('li')
+    //       commandList.appendChild(commandListItem)
+    //       commandListItem.textContent = JSON.stringify(element.data)
+    //   })
+      console.log(`🟢 getCommands(): ${res}`)
       //   return res.data
     } catch (error) {
       console.log(`🔴 getCommands(): ${error}`)
